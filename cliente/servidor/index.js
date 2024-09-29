c95e9124a0c1eeb8b59aff4fd2593a49724971bf
Pedrok390 <pedrocostamelo.henrique91@gmail.com>
2024-09-29 15:28:05 -0300

    csZIJHK

--- cliente/servidor/index.js
+++ cliente/servidor/index.js
@@ -3,7 +3,13 @@ const WebSocket = require("ws");
 const wss = new WebSocket.Server({port: 9082});
 
 wss.on("connection", ws=> {
-    console.log("New Client connected");
+    console.log(`New Client connected ${ws}`);
+
+    ws.on("message", data => {
+        console.log(`Client has sent us: ${data}`);
+
+        ws.send(data.toString());
+    })
 
     ws.on("close", () =>{
         console.log("Client has disconnected");
